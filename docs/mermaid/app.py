24b1befc962bfa7fdd40cbe9fc7fc100705c8783
Roberto Polli <robipolli@gmail.com>
2023-02-08 00:41:34 +0100

    app test.

--- docs/mermaid/app.py
+++ docs/mermaid/app.py
@@ -1,63 +1,14 @@
 import logging
-from pathlib import Path
 from time import time
 
 import pandas as pd
 import yaml
 from rdflib import Graph
-from rdflib.term import Literal, URIRef
 
 import kuberdf
 import mermaidrdf
 
 log = logging.getLogger(__name__)
-
-turtle_text = """
-@prefix : <https://par-tec.it/example#> .
-@prefix d3f: <http://d3fend.mitre.org/ontologies/d3fend.owl#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-
-:Client a :Node ;
-    d3f:accesses :WebMail ;
-    d3f:uses d3f:LoginSession .
-
-:IMAP a d3f:FileSystem,
-        d3f:MailService,
-        :Node ;
-    rdfs:label "IMAP fa:fa-envelope fa:fa-folder" ;
-    d3f:accesses :Mailstore ;
-    d3f:reads :Authorization ;
-    d3f:related d3f:Email .
-
-:Mailstore a d3f:FileSystem,
-        :Node ;
-    rdfs:label "Mailstore fa:fa-envelope fa:fa-folder" ;
-    d3f:related d3f:Email .
-
-:SMTP a d3f:FileSystem,
-        d3f:MailService,
-        d3f:MessageTransferAgent,
-        :Node ;
-    rdfs:label "SMTP fa:fa-envelope fa:fa-folder" ;
-    d3f:reads :Authorization ;
-    d3f:related d3f:Email .
-
-:WebMail a d3f:GraphicalUserInterface,
-        d3f:WebServerApplication,
-        :Node ;
-    rdfs:label "WebMail fab:fa-react fa:fa-envelope" ;
-    d3f:accesses :IMAP,
-        :SMTP ;
-    d3f:related d3f:Email .
-
-:Authorization a :Node ;
-    rdfs:label "d3f:AuthorizationService fa:fa-user-secret" ;
-    d3f:authenticates :Client ;
-    d3f:produces d3f:AuthenticationLog,
-        d3f:LoginSession ;
-    d3f:related d3f:UserAccount .
-
-"""
 
 
 def initialize_graph(ontologies):
@@ -92,15 +43,6 @@ def guess_content(text):
     return None
 
 
-def test_as_graph():
-    g = Graph()
-    g.parse(data=turtle_text, format="turtle")
-    g.parse("../d3fend.ttl", format="turtle")
-    html = d3fend_summary(g)
-    Path("/tmp/foo.html").write_text(html)
-    raise NotImplementedError
-
-
 def d3fend_summary(g: Graph):
     ret = list(
         g.query(
@@ -126,17 +68,10 @@ def d3fend_summary(g: Graph):
     return [headers] + [render_row(row) for row in ret]
 
 
-def test_d3fend_summary_pd():
-    g = initialize_graph(["../d3fend.ttl"])
-    g.parse(data=turtle_text, format="turtle")
-    rows = d3fend_summary(g)
-    df = pd.DataFrame(rows[1:], columns=rows[0])
-    raise NotImplementedError
-
-
 def d3fend_summary_html(g: Graph):
-    summary = d3fend_summary(g)
-    html = list_as_html_table(summary)
+    rows = d3fend_summary(g)
+    df = pd.DataFrame(data=rows[1:], columns=rows[0])
+    html = df.to_html(formatters=[markdown_link_to_html_link] * 4, escape=False)
     return html
 
 
@@ -163,21 +98,6 @@ def markdown_link_to_html_link(markdown_link):
     return markdown_link
 
 
-def test_render_row():
-    testcases = [
-        (
-            URIRef("https://par-tec.it/example#Authorization"),
-            URIRef("http://d3fend.mitre.org/ontologies/d3fend.owl#related"),
-            URIRef("http://d3fend.mitre.org/ontologies/d3fend.owl#UserAccount"),
-            Literal("T1136"),
-            Literal("Create Account"),
-        )
-    ]
-    for row in testcases:
-        render_row(row)
-        raise NotImplementedError
-
-
 def render_row(row):
     def _fix_url(url):
         url = str(url).replace("http://d3fend.mitre.org/ontologies/d3fend.owl#", "d3f:")
@@ -187,9 +107,6 @@ def render_row(row):
     node, relation, artifact, attack_id, attack_label = row
     artifact_name = artifact.split("#")[-1]
     artifact_url = f"https://next.d3fend.mitre.org/dao/artifact/d3f:{artifact_name}"
-    d3f_attack_url = (
-        f"https://next.d3fend.mitre.org/offensive-technique/attack/{attack_id}"
-    )
     attack_url = f"https://attack.mitre.org/techniques/{attack_id}"
     return (
         _fix_url(node),
---
+++ docs/mermaid/test_app.py
@@ -0,0 +1,94 @@
+import tempfile
+
+import pandas as pd
+from rdflib import Graph
+from rdflib.term import Literal, URIRef
+
+from .app import (
+    d3fend_summary,
+    initialize_graph,
+    markdown_link_to_html_link,
+    render_row,
+)
+
+turtle_text = """
+@prefix : <https://par-tec.it/example#> .
+@prefix d3f: <http://d3fend.mitre.org/ontologies/d3fend.owl#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+
+:Client a :Node ;
+    d3f:accesses :WebMail ;
+    d3f:uses d3f:LoginSession .
+
+:IMAP a d3f:FileSystem,
+        d3f:MailService,
+        :Node ;
+    rdfs:label "IMAP fa:fa-envelope fa:fa-folder" ;
+    d3f:accesses :Mailstore ;
+    d3f:reads :Authorization ;
+    d3f:related d3f:Email .
+
+:Mailstore a d3f:FileSystem,
+        :Node ;
+    rdfs:label "Mailstore fa:fa-envelope fa:fa-folder" ;
+    d3f:related d3f:Email .
+
+:SMTP a d3f:FileSystem,
+        d3f:MailService,
+        d3f:MessageTransferAgent,
+        :Node ;
+    rdfs:label "SMTP fa:fa-envelope fa:fa-folder" ;
+    d3f:reads :Authorization ;
+    d3f:related d3f:Email .
+
+:WebMail a d3f:GraphicalUserInterface,
+        d3f:WebServerApplication,
+        :Node ;
+    rdfs:label "WebMail fab:fa-react fa:fa-envelope" ;
+    d3f:accesses :IMAP,
+        :SMTP ;
+    d3f:related d3f:Email .
+
+:Authorization a :Node ;
+    rdfs:label "d3f:AuthorizationService fa:fa-user-secret" ;
+    d3f:authenticates :Client ;
+    d3f:produces d3f:AuthenticationLog,
+        d3f:LoginSession ;
+    d3f:related d3f:UserAccount .
+
+"""
+
+
+def test_render_row():
+    testcases = [
+        (
+            URIRef("https://par-tec.it/example#Authorization"),
+            URIRef("http://d3fend.mitre.org/ontologies/d3fend.owl#related"),
+            URIRef("http://d3fend.mitre.org/ontologies/d3fend.owl#UserAccount"),
+            Literal("T1136"),
+            Literal("Create Account"),
+        )
+    ]
+    for row in testcases:
+        render_row(row)
+        raise NotImplementedError
+
+
+def test_as_graph():
+    g = Graph()
+    g.parse(data=turtle_text, format="turtle")
+    g.parse("../d3fend.ttl", format="turtle")
+    html = d3fend_summary(g)
+    deleteme_html = tempfile.NamedTemporaryFile(suffix=".html")
+    deleteme_html.write_text(html)
+    raise NotImplementedError
+
+
+def test_d3fend_summary_pd():
+    g = initialize_graph(["../d3fend.ttl"])
+    g.parse(data=turtle_text, format="turtle")
+    rows = d3fend_summary(g)
+    df = pd.DataFrame(rows[1:], columns=rows[0])
+    html = df.to_html(formatters=[markdown_link_to_html_link] * 4, escape=False)
+    assert html
+    raise NotImplementedError
